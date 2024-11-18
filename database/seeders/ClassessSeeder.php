bf31928a3a530b5d2db0b9e017ae333bd3c2b679
Clarncz <clarencebelarmino@gmail.com>
2024-11-18 13:58:20 +0800

    Activity 2 - Install preline and display students

--- database/seeders/ClassessSeeder.php
+++ database/seeders/ClassessSeeder.php
@@ -1,10 +1,14 @@
 <?php
+
 namespace Database\Seeders;
+
 use App\Models\Classes;
 use App\Models\Section;
 use App\Models\Student;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
+
 class ClassesSeeder extends Seeder
 {
     /**
@@ -13,28 +17,25 @@ class ClassesSeeder extends Seeder
     public function run(): void
     {
         Classes::factory()
-        ->count(10)
-        ->sequence(fn($sequence) => ['name' => 'Class ' . $sequence->index + 1])
-        ->has(
-            Section::factory()
-                ->count(2)
-                ->state(
-                    new Sequence(
-                        [ 'name' => 'Section A' ],
-                        [ 'name' => 'Section B '],
+            ->count(10)
+            ->sequence(fn($sequence) => ['name' => 'Class ' . $sequence->index + 1])
+            ->has(
+                Section::factory()
+                    ->count(2)
+                    ->state(
+                        new Sequence(
+                            [ 'name' => 'Section A' ],
+                            [ 'name' => 'Section B '],
+                        )
+                    )
+                    ->has(
+                        Student::factory()
+                            ->count(5)
+                            ->state(
+                                fn(array $attributes, Section $section) => ['class_id' => $section->class_id]
+                            )
                     )
                 )
-                ->has(
-                    Student::factory()
-                        ->count(5)
-                        ->state(
-                            function(array $attributes, Section $section)
-                            { 
-                                return ['class_id' => $section->class_id]; 
-                            }
-                        )
-                )
-            )
-        ->create();
+            ->create();
     }
 }
